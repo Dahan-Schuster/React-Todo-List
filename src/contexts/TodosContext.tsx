d672cd5b691de73acbba7602380cf1c033f8583d
Dahan Schuster <dan.plschuster@gmail.com>
2023-08-21 17:46:22 -0300

    feat: add methods for Todos CRUD

--- src/contexts/TodosContext.tsx
+++ src/contexts/TodosContext.tsx
@@ -11,6 +11,10 @@ type TodoType = {
 interface TodosContextValue {
   todosList: TodoType[];
   addTodo: (newTodoDescription: string) => void;
+  removeTodo: (todoId: string) => void;
+  toggleTodoCompleted: (todoId: string) => void;
+  totalItensCreated: number;
+  totalItensCompleted: number;
 }
 
 const TodosContext = React.createContext<TodosContextValue | null>(null);
@@ -24,6 +28,11 @@ export const TodosProvider: React.FC<{ children: React.ReactNode }> = ({
 }) => {
   const [todosList, setTodosList] = React.useState<TodoType[]>([]);
 
+  /**
+   * Método para adicionar itens à lista de Todos
+   * Recebe a descrição do todo e a inicializa como não completada,
+   * usando um uuid aleatório
+   */
   const addTodo = React.useCallback((newTodoDescription: string) => {
     if (!newTodoDescription) return;
 
@@ -33,15 +42,56 @@ export const TodosProvider: React.FC<{ children: React.ReactNode }> = ({
       completed: false,
     };
 
-		console.log(newTodo);
+    console.log(newTodo);
     setTodosList((list) => [...list, newTodo]);
   }, []);
+
+  /**
+   * Método para remover um Todo da lista pelo seu ID
+   */
+  const removeTodo = React.useCallback(
+    (id: string) => {
+      const newList = todosList.filter((item) => item.id !== id);
+      setTodosList(newList);
+    },
+    [todosList]
+  );
+
+  /**
+   * Método para alternar o estado Todo entre completado/não completado pelo seu ID
+   */
+  const toggleTodoCompleted = React.useCallback((id: string) => {
+    setTodosList((list) =>
+      list.map((item) => {
+        if (item.id !== id) return item;
+        return { ...item, completed: !item.completed };
+      })
+    );
+  }, []);
+
+  /**
+   * Memo para armazenar o valor de tarefas criadas no total
+   */
+  const totalItensCreated = React.useMemo(() => {
+    return todosList.length;
+  }, [todosList.length]);
+
+  /**
+   * Memo para armazenar o valor de tarefas completadas no total
+   */
+  const totalItensCompleted = React.useMemo(() => {
+    return todosList.filter((item) => item.completed).length;
+  }, [todosList]);
 
   return (
     <TodosContext.Provider
       value={{
         todosList,
         addTodo,
+        toggleTodoCompleted,
+        removeTodo,
+        totalItensCreated,
+        totalItensCompleted,
       }}
     >
       {children}
