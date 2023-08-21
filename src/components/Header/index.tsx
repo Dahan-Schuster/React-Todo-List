86cba8dcf523082d518ea3331d930523dbaf4ef5
Dahan Schuster <dan.plschuster@gmail.com>
2023-08-21 15:10:46 -0300

    feat: Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import type { FunctionComponent }  from "react";
+
+import styles from "./Header.module.css";
+
+import logo from "../../assets/Logo.svg";
+
+/**
+* Componente de cabeçalho do app
+*/
+const Header: FunctionComponent = () => {
+	return (
+		<div className={styles.header}>
+			<img src={logo} className={styles.logo} />
+		</div>
+	);
+};
+
+export default Header;
