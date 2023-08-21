cc86d68dc0ceb2bf151702deeded866b6a8c04e9
Dahan Schuster <dan.plschuster@gmail.com>
2023-08-21 16:24:02 -0300

    feat: Main section with the CreateTodo container

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import Header from "./components/Header";
 import "./global.css";
 
 import styles from "./App.module.css";
+import CreateTodo from "./containers/CreateTodo";
 
 function App() {
+
   return (
     <div className={styles.body}>
       <Header />
+			<div className={styles.mainSection}>
+				<CreateTodo />
+			</div>
     </div>
   );
 }
