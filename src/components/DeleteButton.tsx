8b394e883c43b88137ccd859239582eaa9d6e967
Dahan Schuster <dan.plschuster@gmail.com>
2023-08-22 16:06:40 -0300

    feat: Confirmação de exclusão no DeleteButton

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -4,19 +4,51 @@ import { FiTrash } from "react-icons/fi";
 import styles from "./DeleteButton.module.css";
 
 interface DeleteButtonProps {
-	onClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  onClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 
 /**
-* Botão usado em situações de deleção de itens
-*/
-const DeleteButton: React.FunctionComponent<DeleteButtonProps> = ({ onClick }) => {
-	return (
-		<button className={styles.button} onClick={onClick}>
-			<FiTrash size={24} />
-		</button>
-	);
+ * Botão usado em situações de deleção de itens
+ */
+const DeleteButton: React.FunctionComponent<DeleteButtonProps> = ({
+  onClick,
+}) => {
+  const [gotFirstClick, setGotFirstClick] = React.useState<boolean>(false);
+
+	React.useEffect(() => {
+		document.addEventListener('click', clearClicks)
+
+		return () => {
+			document.removeEventListener('click', clearClicks)
+		}
+	}, []);
+
+	const clearClicks = React.useCallback(() => {
+		setGotFirstClick(false);
+	}, [])
+
+  const handleClick = React.useCallback(
+    (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+			e.stopPropagation();
+
+      if (!gotFirstClick) {
+        setGotFirstClick(true);
+        return;
+      }
+
+      onClick(e);
+    },
+    [gotFirstClick, onClick]
+  );
+
+  return (
+    <button
+      className={`${styles.button} ${gotFirstClick ? styles.withOneClick : ""}`}
+      onClick={handleClick}
+    >
+      <FiTrash size={24} />
+    </button>
+  );
 };
 
 export default DeleteButton;
-
